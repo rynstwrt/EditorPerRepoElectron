1107dd5df368f0f4e8d191aa74249f2edc9d3d8c
Ryn. <rynstwrt17@gmail.com>
2025-03-09 05:57:25 -0500

    Update preloader documentation and organization

--- src/js/preload.js
+++ src/js/preload.js
@@ -1,38 +1,45 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 
-// MAIN -> RENDERER:
-    // 1) MAIN: win.webContents.send("create-alert", <msgFromMain>);
-    // 2) RENDERER: window.eprGUI.onCreateAlert((msgFromMain) => { ... });
+/*
+MAIN -> RENDERER:
+    1) MAIN: win.webContents.send("create-alert", <msgFromMain>);
+    2) RENDERER: window.eprGUI.onCreateAlert((msgFromMain) => { ... });
+
+RENDERER -> MAIN:
+    1) RENDERER: window.eprGUI.afterCreateAlert(<msgFromRenderer>);
+    2) MAIN: ipcMain.on("after-create-alert", (_event, msgFromRenderer) => { ... });
+
+RENDERER -> MAIN -> RENDERER:
+    1) RENDERER: window.eprGUI.printOnMainFromRenderer(<msgFromRenderer>).then(msgFromMain => alert(msgFromMain));
+    2) MAIN: ipcMain.handle("print-on-main-from-renderer", (_event, msgFromRenderer) => { ...return <msgFromMain>; });
+*/
 
 
-// RENDERER -> MAIN:
-    // 1) RENDERER: window.eprGUI.afterCreateAlert(<msgFromRenderer>);
-    // 2) MAIN: ipcMain.on("after-create-alert", (_event, msgFromRenderer) => { ... });
-
-
-// RENDERER -> MAIN -> RENDERER:
-    // 1) RENDERER: window.eprGUI.printOnMainFromRenderer(<msgFromRenderer>).then(msgFromMain => alert(msgFromMain));
-    // 2) MAIN: ipcMain.handle("print-on-main-from-renderer", (_event, msgFromRenderer) => { ...return <msgFromMain>; });
-
-contextBridge.exposeInMainWorld("eprGUI", {
+const createAlertEntries = {
     // [MAIN -> RENDERER]
-    onCreateAlert: callback => ipcRenderer.on("create-alert", (_event, value) => callback(value)),
-
+    onCreateAlert: callback => ipcRenderer.on("create-alert", (_event, value) => callback(value)),  // M->R
 
     // [RENDERER -> MAIN]
-    afterCreateAlert: afterAlertMsg => ipcRenderer.send("after-create-alert", afterAlertMsg),
+    afterCreateAlert: afterAlertMsg => ipcRenderer.send("after-create-alert", afterAlertMsg),  // R->M
+};
 
 
+const printOnMainFromRendererEntries = {
     // [RENDERER -> MAIN -> RENDERER]
-    printOnMainFromRenderer: msg => ipcRenderer.invoke("print-on-main-from-renderer", msg),
-});
+    printOnMainFromRenderer: msg => ipcRenderer.invoke("print-on-main-from-renderer", msg),  // R->M->R
+};
 
 
-
-// // STOCK EXAMPLE:
+// // [STOCK EXAMPLE]:
 // contextBridge.exposeInMainWorld('versions', {
 //     getNode: () => process.versions.node,
 //     getChrome: () => process.versions.chrome,
 //     getElectron: () => process.versions.electron
 // });
+
+
+contextBridge.exposeInMainWorld("eprGUI", {
+    ...createAlertEntries,
+    ...printOnMainFromRendererEntries
+});
