1c80ee2ac3dcf01bd665be2b963357b7aad1f3b1
Ryn. <rynstwrt17@gmail.com>
2025-03-09 05:45:56 -0500

    Write docs for IPC communication

--- src/js/preload.js
+++ src/js/preload.js
@@ -1,22 +1,38 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 
-contextBridge.exposeInMainWorld('versions', {
-    getNode: () => process.versions.node,
-    getChrome: () => process.versions.chrome,
-    getElectron: () => process.versions.electron
-});
+// MAIN -> RENDERER:
+    // 1) MAIN: win.webContents.send("create-alert", <msgFromMain>);
+    // 2) RENDERER: window.eprGUI.onCreateAlert((msgFromMain) => { ... });
 
+
+// RENDERER -> MAIN:
+    // 1) RENDERER: window.eprGUI.afterCreateAlert(<msgFromRenderer>);
+    // 2) MAIN: ipcMain.on("after-create-alert", (_event, msgFromRenderer) => { ... });
+
+
+// RENDERER -> MAIN -> RENDERER:
+    // 1) RENDERER: window.eprGUI.printOnMainFromRenderer(<msgFromRenderer>).then(msgFromMain => alert(msgFromMain));
+    // 2) MAIN: ipcMain.handle("print-on-main-from-renderer", (_event, msgFromRenderer) => { ...return <msgFromMain>; });
 
 contextBridge.exposeInMainWorld("eprGUI", {
-    // MAIN -> RENDERER //
+    // [MAIN -> RENDERER]
     onCreateAlert: callback => ipcRenderer.on("create-alert", (_event, value) => callback(value)),
 
 
-    // RENDERER -> MAIN (ONE-WAY) //
+    // [RENDERER -> MAIN]
     afterCreateAlert: afterAlertMsg => ipcRenderer.send("after-create-alert", afterAlertMsg),
 
 
-    // RENDERER -> MAIN (TWO-WAYS) //
+    // [RENDERER -> MAIN -> RENDERER]
     printOnMainFromRenderer: msg => ipcRenderer.invoke("print-on-main-from-renderer", msg),
 });
+
+
+
+// // STOCK EXAMPLE:
+// contextBridge.exposeInMainWorld('versions', {
+//     getNode: () => process.versions.node,
+//     getChrome: () => process.versions.chrome,
+//     getElectron: () => process.versions.electron
+// });
