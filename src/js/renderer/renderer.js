3b3d652b7e4d7c7861eb2ce8873ea74df82d1aa0
Ryn. <rynstwrt17@gmail.com>
2025-03-10 11:51:51 -0500

    Prepare for editor storage

--- src/js/renderer/renderer.js
+++ src/js/renderer/renderer.js
@@ -1,20 +1,36 @@
+const editorSelectForm = document.querySelector("#editor-select-form");
 const editorSelect = document.querySelector("#editor-select");
+const addEditorButton = document.querySelector("#add-editor-button");
 
 
-// function addEditor(fileInfo)
-// {
-//     const filePath = fileInfo.filePath;
-//
-//     const selectOption = document.createElement("option");
-//     selectOption.value = encodeURI(filePath);
-//     selectOption.textContent = fileInfo.fileName;
-//
-//     editorSelect.insertBefore(selectOption, editorSelect.firstChild);
-//     editorSelect.value = selectOption.value;
-// }
+editorSelectForm.addEventListener("submit", event =>
+{
+    event.preventDefault();
 
 
-document.querySelector("#add-editor-button").addEventListener("click", async event =>
+    // alert("A")
+    // alert(editorSelect.options[editorSelect.selectedIndex].outerHTML);
+
+    const selectedEditorPath = decodeURI(editorSelect.value).trim();
+    const selectedEditorName = editorSelect.textContent.trim();
+
+
+
+    // alert(selectedEditorName + " " + selectedEditorPath)
+    // alert([encodeURI(selectedEditorPath), selectedEditorPath].join(" ").trim())
+
+    // for (const [key, value] of formData)
+    // {
+    //     console.log(key, value);
+    // }
+
+    // console.log(formData.getAll());
+
+    // event.preventDefault();
+}, false);
+
+
+addEditorButton.addEventListener("click", async event =>
 {
     event.preventDefault();
     const fileInfo = await window["eprAPI"].openFile();
@@ -22,18 +38,9 @@ document.querySelector("#add-editor-button").addEventListener("click", async eve
     const selectOption = document.createElement("option");
     selectOption.value = encodeURI(fileInfo.filePath);
     selectOption.textContent = fileInfo.fileName;
+    // alert(selectOption.value.length, selectOption.textContent.length);
+    // alert(selectOption.value, selectOption.textContent)
 
     editorSelect.insertBefore(selectOption, editorSelect.firstChild);
     editorSelect.value = selectOption.value;
 });
-
-
-// document.querySelector("#add-editor-button").addEventListener("click", event =>
-// {
-//     event.preventDefault();
-//     window["eprAPI"].openFile().then(addEditor);
-// });
-
-
-const allForms = document.querySelectorAll("form");
-allForms.forEach(form => form.addEventListener("submit", event => event.preventDefault()));
