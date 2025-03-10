f169d06649deee78a61160246954eef6a322eb72
Ryn. <rynstwrt17@gmail.com>
2025-03-10 18:55:55 -0500

    Organize again

--- src/main.js
+++ src/main.js
@@ -30,35 +30,38 @@ function createWindow()
 }
 
 
+function createIPCListeners()
+{
+    // Open file select listener
+    ipcMain.handle("dialog:openFile", async () =>
+    {
+        const {canceled, filePaths} = await dialog.showOpenDialog({properties: ["openFile"]});
+        if (canceled)
+            return null;
+
+        // TODO:
+        // const filePath = path.resolve(filePaths[0]);
+        // const fileName = path.basename(filePath);
+        // EPRConfig.addEditor(filePaths[0], path.basename(filePaths[0]));
+        // console.log(EPRConfig.getEditors());
+
+        return {filePath: filePaths[0], fileName: path.basename(filePaths[0])};
+    });
+
+
+    // Open repo in editor listener
+    ipcMain.on("open-repo-in-editor", (_event, data) =>
+    {
+        // TODO: write
+        console.log(data);
+    });
+}
+
+
 app.on("window-all-closed", () =>
 {
     if (process.platform !== "darwin")
         app.quit();
-});
-
-
-// Open file select IPC listener
-ipcMain.handle("dialog:openFile", async () =>
-{
-    const {canceled, filePaths} = await dialog.showOpenDialog({properties: ["openFile"]});
-    if (canceled)
-        return null;
-
-    // TODO:
-    // const filePath = path.resolve(filePaths[0]);
-    // const fileName = path.basename(filePath);
-    // EPRConfig.addEditor(filePaths[0], path.basename(filePaths[0]));
-    // console.log(EPRConfig.getEditors());
-
-    return {filePath: filePaths[0], fileName: path.basename(filePaths[0])};
-});
-
-
-// Open repo in editor IPC listener
-ipcMain.on("open-repo-in-editor", (_event, data) =>
-{
-    // TODO: write
-    console.log(data);
 });
 
 
@@ -71,6 +74,9 @@ app.on("ready", () =>
     app.on("activate", !BrowserWindow.getAllWindows().length
                        ? createWindow
                        : () => {});
+
+    // Create listeners for IPC events
+    createIPCListeners();
 
     // Register developer tools keybind
     globalShortcut.register(
